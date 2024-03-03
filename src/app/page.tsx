3fa77df4f91bcca958cdc28039e9e11c1a73299e
Chia Kwee Cheng <kcchia.2021@scis.smu.edu.sg>
2024-03-03 23:09:15 +0800

    my projects section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import HeroSelection from "./components/HeroSelection";
 import Navbar from "./components/Navbar"
 import AboutSection from "./components/AboutSection"
+import ProjectsSection from "./components/ProjectsSection"
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSelection />
         <AboutSection />
+        <ProjectsSection />
       </div>
     </main>
   );
