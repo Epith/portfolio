7cb65df73c1d19174550090566598350a8c582c4
Chia Kwee Cheng <kcchia.2021@scis.smu.edu.sg>
2024-03-03 20:03:53 +0800

    about me done

--- src/app/components/HeroSelection.jsx
+++ src/app/components/HeroSelection.jsx
@@ -11,13 +11,11 @@ const HeroSelection = () => {
             <div className='col-span-7 place-self-center text-center sm:text-left'>
                 <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extraboldS'>
                   <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
-                    Hello, I'm{" "}
+                    Hello, I'm Joshua{" "}
                     </span>
                     <br></br>
                     <TypeAnimation
                         sequence={[
-                          'Joshua',
-                          1000,
                           'Web Developer',
                           1000,
                           'Mobile Developer',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import HeroSelection from "./components/HeroSelection";
 import Navbar from "./components/Navbar"
+import AboutSection from "./components/AboutSection"
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar/>
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSelection />
+        <AboutSection />
       </div>
     </main>
   );
