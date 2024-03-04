fffcb34c293da101da30a46a20d0dc1a05bd1bbe
Chia Kwee Cheng <kcchia.2021@scis.smu.edu.sg>
2024-03-04 20:40:41 +0800

    email section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import HeroSelection from "./components/HeroSelection";
 import Navbar from "./components/Navbar"
 import AboutSection from "./components/AboutSection"
 import ProjectsSection from "./components/ProjectsSection"
+import EmailSection from "./components/EmailSection"
+import Footer from "./components/Footer"
 
 export default function Home() {
   return (
@@ -12,7 +14,9 @@ export default function Home() {
         <HeroSelection />
         <AboutSection />
         <ProjectsSection />
+        <EmailSection />
       </div>
+      <Footer />
     </main>
   );
 }
