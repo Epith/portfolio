75456d9a70c82409d9c57d6996622528c0e2f232
Chia Kwee Cheng <kcchia.2021@scis.smu.edu.sg>
2024-03-27 22:57:45 +0800

    fix lint error messages

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -40,12 +40,12 @@ const EmailSection = () => {
 
   return (
     <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative'>
-        <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-800 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+        <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-800 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
         <div className="z-10">
-            <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
+            <h5 className='text-xl font-bold text-white my-2'>Let&apos;s Connect</h5>
             <p className='text-[#ADB7BE] mb-4 max-w-md'>
                 {" "}
-                I'm currently looking for new opportunities, feel free to drop me a message!
+                I&apos;m currently looking for new opportunities, feel free to drop me a message!
             </p>
             <div className='socials flex flex-row gap-2'>
                 <Link href="https://github.com/Epith">
@@ -75,7 +75,7 @@ const EmailSection = () => {
                     <textarea name='message' id='message' placeholder='Leave your message here' className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'>
                     </textarea>
                 </div>
-                <button type='submit' className='bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'> Send Message</button>
+                <button type='submit' className='bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'> Send Message</button>
                 {
                     emailSubmitted && (
                         <p className="text-green-500 text-sm mt-2">
